36e6f8c6aa837f30924354819aef5b3fa9a9a4c2
Edward Anthony <icenel.private@gmail.com>
2018-06-13 15:40:20 +0700

    Initial Commit

---
+++ TestingEntity/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestingEntity
+//
+//  Created by Edward Anthony on 13/06/18.
+//  Copyright © 2018 Edward Anthony. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestingEntity/Document.swift
@@ -0,0 +1,28 @@
+//
+//  Document.swift
+//  TestingEntity
+//
+//  Created by Edward Anthony on 13/06/18.
+//  Copyright © 2018 Edward Anthony. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override var windowNibName: NSNib.Name? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return NSNib.Name("Document")
+    }
+
+}
